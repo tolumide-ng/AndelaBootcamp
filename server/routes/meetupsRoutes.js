34c833e59bf738c9e2d3723243b2d6fd231d486d
Tolumide <tolumideshopein@gmail.com>
2019-01-07 16:59:29 +0100

    [Feature #163025365] Add code to fetch all meetups

--- server/routes/meetupsRoutes.js
+++ server/routes/meetupsRoutes.js
@@ -4,5 +4,6 @@ import meetups from '../controllers/meetupsControllers';
 const router = express.Router();
 
 router.post('/', meetups.createMeetup);
+router.get('/', meetups.findAll)
 console.log('welocmer');
 export default router;
